d4f6018cd0eddd9703ed483451a496d522dff582
luckydrq <drqzju@gmail.com>
2015-01-21 00:04:51 +0800

    finish create table

--- index.js
+++ index.js
@@ -2,21 +2,33 @@ var slice = Array.prototype.slice;
 
 function createTable() {
   var arrays = slice.call(arguments);
+  var len = arrays.length;
   var maxLevel = arrays.length;
+  var rowspan = 1;
+
+  for (i = len - 1; i >= 0; i--) {
+    arrays[i].rowspan = rowspan;
+    rowspan *= arrays[i].length;
+  }
 
   function dig(level) {
     var arr = arrays[level++];
     var i, j, tuples = [];
+    var rowspan = arr.rowspan;
 
     if (level === maxLevel) {
       for (i = 0; i < arr.length; i++) {
-        tuples.push([arr[i]]);
+        tuples.push(['<td>', arr[i], '</td>'].join(''));
       }
     } else {
       var subTuples = dig(level);
       for (i = 0; i < arr.length; i++) {
         for (j = 0; j < subTuples.length; j++) {
-          tuples.push([arr[i]].concat(subTuples[j]));
+          if (j % rowspan === 0) {
+            tuples.push('<td rowspan=' + rowspan + '>' + arr[i] + '</td>' + subTuples[j]);
+          } else {
+            tuples.push(subTuples[j]);
+          }
         }
       }
     }
@@ -24,7 +36,7 @@ function createTable() {
     return tuples;
   }
 
-  return dig(0);
+  return '<tr>' + dig(0).join('</tr><tr>') + '</tr>';
 }
 
 var A = ['A1', 'A2'];
